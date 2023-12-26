86c55954cbcf9b9c4c79867ab8cea421b2f74c8e
IotaBread <iotabread@gmail.com>
2023-12-26 17:48:35 -0300

    Move some code to instance methods & fields

--- src/main/java/org/quiltmc/enigma_plugin/index/constant_fields/ConstantFieldNameFinder.java
+++ src/main/java/org/quiltmc/enigma_plugin/index/constant_fields/ConstantFieldNameFinder.java
@@ -22,7 +22,9 @@ import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.FieldInsnNode;
 import org.objectweb.asm.tree.LdcInsnNode;
 import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.analysis.Analyzer;
+import org.objectweb.asm.tree.analysis.AnalyzerException;
 import org.objectweb.asm.tree.analysis.SourceInterpreter;
 import org.objectweb.asm.tree.analysis.SourceValue;
 import org.quiltmc.enigma.api.translation.representation.TypeDescriptor;
@@ -32,10 +34,15 @@ import org.tinylog.Logger;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class ConstantFieldNameFinder implements Opcodes {
+	private final HashMap<String, Set<String>> usedNamesByClass = new HashMap<>();
+	private final HashMap<String, Set<String>> duplicatedNamesByClass = new HashMap<>();
+	private final HashMap<FieldEntry, FieldEntry> linkedFields = new HashMap<>();
+
 	private static boolean isClassPutStatic(String owner, AbstractInsnNode insn) {
 		return insn.getOpcode() == PUTSTATIC && ((FieldInsnNode) insn).owner.equals(owner);
 	}
@@ -46,16 +53,6 @@ public class ConstantFieldNameFinder implements Opcodes {
 
 	private static boolean isCharacterUsable(char c) {
 		return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9' || c == '_';
-	}
-
-	private static FieldEntry followFieldLink(FieldEntry field, Map<FieldEntry, FieldEntry> linkedFields, Map<FieldEntry, String> names) {
-		if (names.containsKey(field)) {
-			return field;
-		} else if (linkedFields.containsKey(field)) {
-			return followFieldLink(linkedFields.get(field), linkedFields, names);
-		}
-
-		return null;
 	}
 
 	private static String stringToUpperSnakeCase(String s) {
@@ -87,154 +84,178 @@ public class ConstantFieldNameFinder implements Opcodes {
 		return usableName.toString().toUpperCase();
 	}
 
+	private FieldEntry followFieldLink(FieldEntry field, Map<FieldEntry, String> names) {
+		if (names.containsKey(field)) {
+			return field;
+		} else if (this.linkedFields.containsKey(field)) {
+			return followFieldLink(this.linkedFields.get(field), names);
+		}
+
+		return null;
+	}
+
+	private void clear() {
+		this.usedNamesByClass.clear();
+		this.duplicatedNamesByClass.clear();
+		this.linkedFields.clear();
+	}
+
 	public Map<FieldEntry, String> findNames(ConstantFieldIndex fieldIndex) throws Exception {
+		this.clear();
+
 		Analyzer<SourceValue> analyzer = new Analyzer<>(new SourceInterpreter());
 		Map<FieldEntry, String> fieldNames = new HashMap<>();
-		Map<String, Set<String>> usedFieldNames = new HashMap<>();
-		Map<String, Set<String>> duplicatedFieldNames = new HashMap<>();
-		Map<FieldEntry, FieldEntry> linkedFields = new HashMap<>();
 
 		for (String clazz : fieldIndex.getStaticInitializers().keySet()) {
 			var initializers = fieldIndex.getStaticInitializers().get(clazz);
 
-			for (var initializer : initializers) {
-				var frames = analyzer.analyze(clazz, initializer);
-				var instructions = initializer.instructions;
+			findNamesInInitializers(clazz, initializers, analyzer, fieldNames);
+		}
 
-				for (int i = 1; i < instructions.size(); i++) {
-					var insn = instructions.get(i);
-					var prevInsn = insn.getPrevious();
+		// Insert linked names
+		for (FieldEntry linked : this.linkedFields.keySet()) {
+			FieldEntry target = followFieldLink(linked, fieldNames);
+			String name = fieldNames.get(target);
 
-					if (!isClassPutStatic(clazz, insn)) {
-						continue;
+			String clazz = linked.getParent().getFullName();
+			Set<String> usedNames = this.usedNamesByClass.computeIfAbsent(clazz, s -> new HashSet<>());
+			Set<String> duplicatedNames = this.duplicatedNamesByClass.computeIfAbsent(clazz, s -> new HashSet<>());
+			if (!duplicatedNames.contains(name) && usedNames.add(name)) {
+				fieldNames.put(linked, name);
+			} else {
+				duplicatedNames.add(name);
+				Logger.warn("Duplicate name \"{}\" for field {}", name, linked);
+			}
+		}
+
+		return fieldNames;
+	}
+
+	private void findNamesInInitializers(String clazz, List<MethodNode> initializers, Analyzer<SourceValue> analyzer, Map<FieldEntry, String> fieldNames) throws AnalyzerException {
+		var usedNames = this.usedNamesByClass.computeIfAbsent(clazz, s -> new HashSet<>());
+		var duplicatedNames = this.duplicatedNamesByClass.computeIfAbsent(clazz, s -> new HashSet<>());
+
+		for (var initializer : initializers) {
+			var frames = analyzer.analyze(clazz, initializer);
+			var instructions = initializer.instructions;
+
+			for (int i = 1; i < instructions.size(); i++) {
+				var insn = instructions.get(i);
+				var prevInsn = insn.getPrevious();
+
+				if (!isClassPutStatic(clazz, insn)) {
+					continue;
+				}
+
+				/*
+				 * We want instructions in the form of
+				 * prevInsn: INVOKESTATIC ${clazz}.* (*)L*; || INVOKESPECIAL ${clazz}.<init> (*)V
+				 * insn:     PUTSTATIC ${clazz}.* : L*;
+				 */
+				var putStatic = (FieldInsnNode) insn;
+				if (!(prevInsn instanceof MethodInsnNode invokeInsn) || !invokeInsn.owner.equals(clazz)) {
+					continue; // Ensure the previous instruction was an invocation of one of this class' methods
+				}
+
+				if (invokeInsn.getOpcode() != INVOKESTATIC && !isInit(invokeInsn)) {
+					continue; // Ensure the invocation is either an INVOKESTATIC or a constructor invocation
+				}
+
+				// Search for a name within the frame for the invocation instruction
+				var frame = frames[i - 1];
+				String name = null;
+				for (int j = 0; j < frame.getStackSize(); j++) {
+					var value = frame.getStack(j);
+					for (var stackInsn : value.insns) {
+						if (stackInsn instanceof LdcInsnNode ldc && ldc.cst instanceof String constant && !constant.isBlank()) {
+							name = constant;
+							break;
+						}
 					}
+				}
 
-					/*
-					 * We want instructions in the form of
-					 * prevInsn: INVOKESTATIC ${clazz}.* (*)L*; || INVOKESPECIAL ${clazz}.<init> (*)V
-					 * insn:     PUTSTATIC ${clazz}.* : L*;
-					 */
-					var putStatic = (FieldInsnNode) insn;
-					if (!(prevInsn instanceof MethodInsnNode invokeInsn) || !invokeInsn.owner.equals(clazz)) {
-						continue; // Ensure the previous instruction was an invocation of one of this class' methods
-					}
-
-					if (invokeInsn.getOpcode() != INVOKESTATIC && !isInit(invokeInsn)) {
-						continue; // Ensure the invocation is either an INVOKESTATIC or a constructor invocation
-					}
-
-					// Search for a name within the frame for the invocation instruction
-					var frame = frames[i - 1];
-					String name = null;
+				if (name == null) {
+					// If we couldn't find a name, try to link this field to one from another class instead
+					FieldInsnNode otherFieldInsn = null;
 					for (int j = 0; j < frame.getStackSize(); j++) {
 						var value = frame.getStack(j);
+						AbstractInsnNode lastStackInsn = null;
 						for (var stackInsn : value.insns) {
-							if (stackInsn instanceof LdcInsnNode ldc && ldc.cst instanceof String constant && !constant.isBlank()) {
-								name = constant;
+							lastStackInsn = stackInsn;
+							if (stackInsn instanceof FieldInsnNode fieldInsn && fieldInsn.getOpcode() == GETSTATIC && !fieldInsn.owner.equals(clazz)) {
+								otherFieldInsn = fieldInsn;
 								break;
 							}
 						}
-					}
 
-					if (name == null) {
-						// If we couldn't find a name, try to link this field to one from another class instead
-						FieldInsnNode otherFieldInsn = null;
-						for (int j = 0; j < frame.getStackSize(); j++) {
-							var value = frame.getStack(j);
-							AbstractInsnNode lastStackInsn = null;
-							for (var stackInsn : value.insns) {
-								lastStackInsn = stackInsn;
+						/* Search between the last stack instruction and the invocation instruction, useful for parameters passed to a constructor
+						 * lastStackInsn: NEW * // Last instruction in the stack
+						 *                DUP
+						 *                GETSTATIC !${clazz}.* : * // Instruction we are looking for
+						 *                INVOKESPECIAL *.<init> (*)V
+						 * invokeInsn:    INVOKESPECIAL ${clazz}.<init> (L*;*)V
+						 */
+						if (otherFieldInsn == null && lastStackInsn != null) {
+							var stackInsn = lastStackInsn;
+							while ((stackInsn = stackInsn.getNext()) != null && stackInsn != invokeInsn) {
 								if (stackInsn instanceof FieldInsnNode fieldInsn && fieldInsn.getOpcode() == GETSTATIC && !fieldInsn.owner.equals(clazz)) {
 									otherFieldInsn = fieldInsn;
 									break;
 								}
 							}
-
-							/* Search between the last stack instruction and the invocation instruction, useful for parameters passed to a constructor
-							 * lastStackInsn: NEW * // Last instruction in the stack
-							 *                DUP
-							 *                GETSTATIC !${clazz}.* : * // Instruction we are looking for
-							 *                INVOKESPECIAL *.<init> (*)V
-							 * invokeInsn:    INVOKESPECIAL ${clazz}.<init> (L*;*)V
-							 */
-							if (otherFieldInsn == null && lastStackInsn != null) {
-								var stackInsn = lastStackInsn;
-								while ((stackInsn = stackInsn.getNext()) != null && stackInsn != invokeInsn) {
-									if (stackInsn instanceof FieldInsnNode fieldInsn && fieldInsn.getOpcode() == GETSTATIC && !fieldInsn.owner.equals(clazz)) {
-										otherFieldInsn = fieldInsn;
-										break;
-									}
-								}
-							}
-						}
-
-						if (otherFieldInsn != null) {
-							linkedFields.put(fieldFromInsn(putStatic), fieldFromInsn(otherFieldInsn));
-						}
-
-						continue; // Done with the current putStatic
-					}
-
-					// Remove identifier namespace
-					if (name.contains(":")) {
-						name = name.substring(name.lastIndexOf(":") + 1);
-					}
-
-					// Process a path
-					if (name.contains("/")) {
-						int separator = name.indexOf("/");
-						String first = name.substring(0, separator);
-						String last;
-
-						if (name.contains(".") && name.indexOf(".") > separator) {
-							last = name.substring(separator + 1, name.indexOf("."));
-						} else {
-							last = name.substring(separator + 1);
-						}
-
-						if (first.endsWith("s")) {
-							first = first.substring(0, first.length() - 1);
-						}
-
-						name = last + "_" + first;
-					}
-
-					var fieldName = stringToUpperSnakeCase(name);
-
-					if (fieldName == null || fieldName.isEmpty()) {
-						continue;
-					}
-
-					Set<String> usedNames = usedFieldNames.computeIfAbsent(clazz, k -> new HashSet<>());
-					Set<String> duplicatedNames = duplicatedFieldNames.computeIfAbsent(clazz, k -> new HashSet<>());
-
-					if (!duplicatedNames.contains(fieldName)) {
-						if (!usedNames.add(fieldName)) {
-							Logger.warn("Duplicate key: " + fieldName + " (" + name + ") in " + clazz);
-							duplicatedNames.add(fieldName);
-							usedNames.remove(fieldName);
 						}
 					}
 
-					if (usedNames.contains(fieldName)) {
-						fieldNames.put(fieldFromInsn(putStatic), fieldName);
+					if (otherFieldInsn != null) {
+						this.linkedFields.put(fieldFromInsn(putStatic), fieldFromInsn(otherFieldInsn));
 					}
+
+					continue; // Done with the current putStatic
+				}
+
+				// Remove identifier namespace
+				if (name.contains(":")) {
+					name = name.substring(name.lastIndexOf(":") + 1);
+				}
+
+				// Process a path
+				if (name.contains("/")) {
+					int separator = name.indexOf("/");
+					String first = name.substring(0, separator);
+					String last;
+
+					if (name.contains(".") && name.indexOf(".") > separator) {
+						last = name.substring(separator + 1, name.indexOf("."));
+					} else {
+						last = name.substring(separator + 1);
+					}
+
+					if (first.endsWith("s")) {
+						first = first.substring(0, first.length() - 1);
+					}
+
+					name = last + "_" + first;
+				}
+
+				var fieldName = stringToUpperSnakeCase(name);
+
+				if (fieldName == null || fieldName.isEmpty()) {
+					continue;
+				}
+
+				if (!duplicatedNames.contains(fieldName)) {
+					if (!usedNames.add(fieldName)) {
+						Logger.warn("Duplicate key: " + fieldName + " (" + name + ") in " + clazz);
+						duplicatedNames.add(fieldName);
+						usedNames.remove(fieldName);
+					}
+				}
+
+				if (usedNames.contains(fieldName)) {
+					fieldNames.put(fieldFromInsn(putStatic), fieldName);
 				}
 			}
 		}
-
-		// Insert linked names
-		for (FieldEntry linked : linkedFields.keySet()) {
-			FieldEntry target = followFieldLink(linked, linkedFields, fieldNames);
-			String name = fieldNames.get(target);
-
-			Set<String> usedNames = usedFieldNames.computeIfAbsent(linked.getParent().getFullName(), s -> new HashSet<>());
-			if (usedNames.add(name)) {
-				fieldNames.put(linked, name);
-			}
-		}
-
-		return fieldNames;
 	}
 
 	private static FieldEntry fieldFromInsn(FieldInsnNode insn) {
