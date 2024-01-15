29fdba9e5d42c04aebeb9689a16c7a0ab2ea585b
IotaBread <iotabread@gmail.com>
2024-01-14 23:10:06 -0300

    Remove some stuff

--- src/main/java/org/quiltmc/enigma_plugin/index/DelegateParametersIndex.java
+++ src/main/java/org/quiltmc/enigma_plugin/index/DelegateParametersIndex.java
@@ -8,22 +8,16 @@ import org.objectweb.asm.tree.InvokeDynamicInsnNode;
 import org.objectweb.asm.tree.LdcInsnNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
-import org.objectweb.asm.tree.TryCatchBlockNode;
 import org.objectweb.asm.tree.analysis.Analyzer;
 import org.objectweb.asm.tree.analysis.AnalyzerException;
-import org.objectweb.asm.tree.analysis.Frame;
 import org.objectweb.asm.tree.analysis.Interpreter;
 import org.objectweb.asm.tree.analysis.Value;
-import org.objectweb.asm.util.Textifier;
-import org.objectweb.asm.util.TraceMethodVisitor;
 import org.quiltmc.enigma.api.class_provider.ClassProvider;
 import org.quiltmc.enigma.api.translation.representation.entry.LocalVariableEntry;
 import org.quiltmc.enigma.api.translation.representation.entry.MethodEntry;
 import org.quiltmc.enigma_plugin.Arguments;
 import org.tinylog.Logger;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -49,9 +43,6 @@ public class DelegateParametersIndex extends Index {
 	}
 
 	public void visitMethodNode(ClassNode classNode, MethodNode node) throws AnalyzerException {
-		// if (classNode.name.equals("com/a/b") || "com/a/b".equals(classNode.nestHostClass)) {
-			Logger.info(classNode.name + "." + node.name + node.desc);
-		// } else {return;}
 		var methodEntry = MethodEntry.parse(classNode.name, node.name, node.desc);
 
 		var frames = new Analyzer<>(new LocalVariableInterpreter()).analyze(classNode.name, node);
@@ -70,7 +61,6 @@ public class DelegateParametersIndex extends Index {
 					var index = j + (isStatic ? 0 : 1);
 
 					if (value.parameter) {
-						Logger.info("{} uses local {} at {}", PrintInterpreter.text(methodInsn), value.local, index);
 						this.linkedParameters.put(new LocalVariableEntry(methodEntry, value.local), new LocalVariableEntry(entry, index));
 					}
 				}
@@ -193,111 +183,6 @@ public class DelegateParametersIndex extends Index {
 		@Override
 		public LocalVariableValue merge(LocalVariableValue value1, LocalVariableValue value2) {
 			return new LocalVariableValue(Math.min(value1.size, value2.size));
-		}
-	}
-
-	@Deprecated
-	public static class PrintInterpreter extends LocalVariableInterpreter {
-		private static final Textifier textifier = new Textifier();
-		private static final TraceMethodVisitor visitor = new TraceMethodVisitor(textifier);
-
-		public PrintInterpreter() {
-			super();
-		}
-
-		public static String text(AbstractInsnNode insn) {
-			insn.accept(visitor);
-			var str = new StringWriter();
-			textifier.print(new PrintWriter(str));
-			textifier.text.clear();
-			return str.toString().trim();
-		}
-
-		public static String text(LocalVariableValue value) {
-			return value.local + ": " + value.size + " (" + value.parameter + ")";
-		}
-
-		private static String text(List<? extends LocalVariableValue> values) {
-			var str = new StringBuilder("[");
-			if (!values.isEmpty()) {
-				for (var val : values) {
-					str.append(text(val)).append(", ");
-				}
-				str.setLength(str.length() - 2);
-			}
-			str.append("]");
-			return str.toString();
-		}
-
-		@Override
-		public LocalVariableValue newOperation(AbstractInsnNode insn) {
-			Logger.info("newOperation({})", text(insn));
-			return super.newOperation(insn);
-		}
-
-		@Override
-		public LocalVariableValue copyOperation(AbstractInsnNode insn, LocalVariableValue value) {
-			Logger.info("copyOperation({}, {})", text(insn), text(value));
-			return super.copyOperation(insn, value);
-		}
-
-		@Override
-		public LocalVariableValue unaryOperation(AbstractInsnNode insn, LocalVariableValue value) {
-			Logger.info("unaryOperation({}, {})", text(insn), text(value));
-			return super.unaryOperation(insn, value);
-		}
-
-		@Override
-		public LocalVariableValue binaryOperation(AbstractInsnNode insn, LocalVariableValue value1, LocalVariableValue value2) {
-			Logger.info("binaryOperation({}, {}, {})", text(insn), text(value1), text(value2));
-			return super.binaryOperation(insn, value1, value2);
-		}
-
-		@Override
-		public LocalVariableValue ternaryOperation(AbstractInsnNode insn, LocalVariableValue value1, LocalVariableValue value2, LocalVariableValue value3) {
-			Logger.info("ternaryOperation({}, {}, {})", text(insn), text(value1), text(value2), text(value3));
-			return super.ternaryOperation(insn, value1, value2, value3);
-		}
-
-		@Override
-		public LocalVariableValue naryOperation(AbstractInsnNode insn, List<? extends LocalVariableValue> values) {
-			Logger.info("naryOperation({}, {})", text(insn), text(values));
-			return super.naryOperation(insn, values);
-		}
-
-		@Override
-		public void returnOperation(AbstractInsnNode insn, LocalVariableValue value, LocalVariableValue expected) {
-			Logger.info("returnOperation({}, {}, {})", text(insn), text(value), text(expected));
-			super.returnOperation(insn, value, expected);
-		}
-
-		@Override
-		public LocalVariableValue merge(LocalVariableValue value1, LocalVariableValue value2) {
-			Logger.info("merge({}, {})", text(value1), text(value2));
-			return super.merge(value1, value2);
-		}
-
-		@Override
-		public LocalVariableValue newParameterValue(boolean isInstanceMethod, int local, Type type) {
-			Logger.info("newParameterValue({}, {}, {})", isInstanceMethod, local, type);
-			return super.newParameterValue(isInstanceMethod, local, type);
-		}
-
-		@Override
-		public LocalVariableValue newReturnTypeValue(Type type) {
-			Logger.info("newReturnTypeValue({})", type);
-			return super.newReturnTypeValue(type);
-		}
-
-		@Override
-		public LocalVariableValue newEmptyValue(int local) {
-			Logger.info("newEmptyValue({})", local);
-			return super.newEmptyValue(local);
-		}
-
-		@Override
-		public LocalVariableValue newExceptionValue(TryCatchBlockNode tryCatchBlockNode, Frame<LocalVariableValue> handlerFrame, Type exceptionType) {
-			return super.newExceptionValue(tryCatchBlockNode, handlerFrame, exceptionType);
 		}
 	}
 }
