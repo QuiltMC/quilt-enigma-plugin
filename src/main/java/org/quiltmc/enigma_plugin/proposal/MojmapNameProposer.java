f0981e48927729df9596546ab76c464b7615f5ae
ix0rai <ix0rai64@gmail.com>
2024-12-17 21:12:30 -0600

    initial package json gen impl

--- src/main/java/org/quiltmc/enigma_plugin/proposal/MojmapNameProposer.java
+++ src/main/java/org/quiltmc/enigma_plugin/proposal/MojmapNameProposer.java
@@ -1,19 +1,24 @@
 package org.quiltmc.enigma_plugin.proposal;
 
+import com.google.gson.GsonBuilder;
 import org.quiltmc.enigma.api.Enigma;
-import org.quiltmc.enigma.api.analysis.index.jar.EntryIndex;
+import org.quiltmc.enigma.api.ProgressListener;
 import org.quiltmc.enigma.api.analysis.index.jar.JarIndex;
+import org.quiltmc.enigma.api.analysis.index.mapping.MappingsIndex;
+import org.quiltmc.enigma.api.analysis.index.mapping.PackageIndex;
 import org.quiltmc.enigma.api.translation.mapping.EntryMapping;
-import org.quiltmc.enigma.api.translation.mapping.EntryRemapper;
 import org.quiltmc.enigma.api.translation.mapping.tree.EntryTree;
 import org.quiltmc.enigma.api.translation.mapping.tree.EntryTreeNode;
-import org.quiltmc.enigma.api.translation.representation.entry.ClassEntry;
 import org.quiltmc.enigma.api.translation.representation.entry.Entry;
 import org.tinylog.Logger;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 
 public class MojmapNameProposer extends NameProposer {
@@ -48,6 +53,7 @@ public class MojmapNameProposer extends NameProposer {
 		});
 
 		if (mojmaps != null) {
+			var json = createPackageJson();
 			mojmaps.getRootNodes().forEach((node) -> this.proposeNodeAndChildren(mappings, node));
 		}
 	}
@@ -58,5 +64,108 @@ public class MojmapNameProposer extends NameProposer {
 		}
 
 		node.getChildNodes().forEach((child) -> proposeNodeAndChildren(mappings, child));
+	}
+
+	@SuppressWarnings("OptionalGetWithoutIsPresent")
+	public static String createPackageJson() {
+		MappingsIndex index = new MappingsIndex(new PackageIndex());
+		index.indexMappings(mojmaps, ProgressListener.createEmpty());
+
+		var packageNames = index.getIndex(PackageIndex.class).getPackageNames();
+		List<PackageEntry> rootPackages = new ArrayList<>();
+
+		Map<Integer, List<String>> packageNamesByDepth = new HashMap<>();
+
+		for (String packageName : new ArrayList<>(packageNames)) {
+			int slashes;
+
+			if (!packageName.contains("/")) {
+				slashes = 0;
+			} else {
+				slashes = packageName.split("/").length - 1;
+			}
+
+			packageNamesByDepth.computeIfAbsent(slashes, k -> new ArrayList<>()).add(packageName);
+		}
+
+		for (int i = 0; i < packageNamesByDepth.keySet().stream().mapToInt(num -> num).max().getAsInt(); i++) {
+			List<String> names = packageNamesByDepth.get(i);
+
+			if (i == 0) {
+				names.forEach(name -> rootPackages.add(new PackageEntry(name, "")));
+			} else {
+				for (String name : names) {
+					String rootName = name.split("/")[0];
+
+					PackageEntry rootPackage = rootPackages.stream().filter(entry -> entry.obf.equals(rootName)).findFirst().orElse(null);
+					if (rootPackage != null) {
+						String packageUp = name.substring(0, name.lastIndexOf('/'));
+
+						PackageEntry parent = rootPackage.findEntry(packageUp);
+						if (parent != null) {
+							String finalPackage = name.substring(name.lastIndexOf('/') + 1);
+							PackageEntry child = new PackageEntry(finalPackage, "");
+							child.parent = parent;
+							parent.children.add(child);
+						}
+					}
+				}
+			}
+		}
+
+		return new GsonBuilder().setPrettyPrinting().create().toJson(rootPackages);
+	}
+
+	private static class PackageEntry {
+		public String obf;
+		public String deobf;
+		public List<PackageEntry> children;
+		private transient PackageEntry parent;
+
+		public PackageEntry(String obf, String deobf) {
+			this.obf = obf;
+			this.deobf = deobf;
+			this.children = new ArrayList<>();
+		}
+
+		public PackageEntry findEntry(String obf) {
+			if (this.equals(obf)) {
+				return this;
+			}
+
+			for (PackageEntry entry : this.children) {
+				var found = entry.findEntry(obf);
+				if (found != null) {
+					return found;
+				}
+			}
+
+			return null;
+		}
+
+		private boolean equals(String obfPackage) {
+			String[] packages = obfPackage.split("/");
+			Collections.reverse(Arrays.asList(packages));
+
+			return this.checkRecursively(packages);
+		}
+
+		private boolean checkRecursively(String[] packageNames) {
+			PackageEntry checkedPackage = this;
+
+			for (String packageName : packageNames) {
+				if (checkedPackage == null) {
+					return true;
+				}
+
+				if (!checkedPackage.obf.equals(packageName)) {
+					return false;
+				}
+
+				checkedPackage = checkedPackage.parent;
+			}
+
+			return true;
+		}
 	}
 }
