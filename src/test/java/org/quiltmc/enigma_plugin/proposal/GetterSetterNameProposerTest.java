b2d1c98f99d21c17151140a5be3b177188522f4f
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-09-06 13:12:32 -0700

    move GetterSetterNameProposerTest to stand-alone test class

---
+++ src/test/java/org/quiltmc/enigma_plugin/proposal/GetterSetterNameProposerTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2024 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.quiltmc.enigma_plugin.proposal;
+
+import org.junit.jupiter.api.Test;
+import org.quiltmc.enigma.api.translation.mapping.EntryMapping;
+import org.quiltmc.enigma.api.translation.representation.entry.ClassEntry;
+import org.quiltmc.enigma.api.translation.representation.entry.MethodEntry;
+import org.quiltmc.enigma.util.validation.ValidationContext;
+import org.quiltmc.enigma_plugin.util.ProposalAsserter;
+import org.quiltmc.enigma_plugin.util.TestUtil;
+
+import java.nio.file.Path;
+
+import static org.quiltmc.enigma_plugin.util.TestUtil.field;
+import static org.quiltmc.enigma_plugin.util.TestUtil.localVar;
+import static org.quiltmc.enigma_plugin.util.TestUtil.method;
+
+public class GetterSetterNameProposerTest {
+	private static final Path JAR = TestUtil.obfJarPathOf("GetterSetterTest-obf");
+
+	@Test
+	public void testGetterSetterNames() {
+		final ProposalAsserter asserter = new ProposalAsserter(TestUtil.setupEnigma(JAR), GetterSetterNameProposer.ID);
+
+		var getterSetterTest = new ClassEntry("a/a/a");
+
+		var context = new ValidationContext(null);
+		asserter.remapper().putMapping(context, field(getterSetterTest, "a", "I"), new EntryMapping("silliness"));
+		asserter.remapper().putMapping(context, field(getterSetterTest, "b", "Ljava/lang/String;"), new EntryMapping("name"));
+
+		asserter.assertDynamicProposal("getSilliness", method(getterSetterTest, "a", "()I"));
+
+		final MethodEntry setSilliness = method(getterSetterTest, "a", "(I)V");
+		asserter.assertDynamicProposal("setSilliness", setSilliness);
+		asserter.assertDynamicProposal("silliness", localVar(setSilliness, 1));
+
+		asserter.assertDynamicProposal("getName", method(getterSetterTest, "b", "()Ljava/lang/String;"));
+
+		final MethodEntry setName = method(getterSetterTest, "b", "(Ljava/lang/String;)V");
+		asserter.assertDynamicProposal("setName", setName);
+		asserter.assertDynamicProposal("name", localVar(setName, 1));
+	}
+}
