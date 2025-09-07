e777ce9ee6d66d53d0ee35ed1c0c47c34d56f245
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2025-09-06 18:17:54 -0700

    make DelegateParametersNameProposerTest descriptors DRYer

--- src/test/java/org/quiltmc/enigma_plugin/proposal/DelegateParametersNameProposerTest.java
+++ src/test/java/org/quiltmc/enigma_plugin/proposal/DelegateParametersNameProposerTest.java
@@ -21,73 +21,88 @@ import org.quiltmc.enigma.api.translation.mapping.EntryMapping;
 import org.quiltmc.enigma.api.translation.representation.entry.ClassEntry;
 import org.quiltmc.enigma.api.translation.representation.entry.MethodEntry;
 import org.quiltmc.enigma.util.validation.ValidationContext;
+import org.quiltmc.enigma_plugin.util.CommonDescriptors;
 import org.quiltmc.enigma_plugin.util.ProposalAsserter;
 import org.quiltmc.enigma_plugin.util.TestUtil;
 
 import java.nio.file.Path;
 
 import static org.quiltmc.enigma_plugin.util.TestUtil.localVar;
-import static org.quiltmc.enigma_plugin.util.TestUtil.method;
+import static org.quiltmc.enigma_plugin.util.TestUtil.methodOf;
+import static org.quiltmc.enigma_plugin.util.TestUtil.typeDescOf;
 
-public class DelegateParametersNameProposerTest {
+public class DelegateParametersNameProposerTest implements CommonDescriptors {
 	private static final Path JAR = TestUtil.obfJarPathOf("DELEGATE_PARAMETERS-obf");
+	private static final String DELEGATE_PARAMETERS_TEST_NAME = "a/a/a";
+	private static final String TEST_1 = typeDescOf(DELEGATE_PARAMETERS_TEST_NAME + "$a");
 
 	@Test
 	public void testDelegateParameterNames() {
 		final ProposalAsserter asserter = new ProposalAsserter(TestUtil.setupEnigma(JAR), DelegateParametersNameProposer.ID);
 
-		final var delegateParametersTest = new ClassEntry("a/a/a");
+		final var delegateParametersTest = new ClassEntry(DELEGATE_PARAMETERS_TEST_NAME);
 
-		asserter.assertDynamicProposal("seed", localVar(method(delegateParametersTest, "a", "(J)I"), 1));
-		asserter.assertDynamicProposal("seed", localVar(method(delegateParametersTest, "a", "(I)V"), 1));
-		asserter.assertDynamicProposal("seed", localVar(method(delegateParametersTest, "b", "(J)V"), 1));
+		asserter.assertDynamicProposal("seed", localVar(methodOf(delegateParametersTest, "a", I, J), 1));
+		asserter.assertDynamicProposal("seed", localVar(methodOf(delegateParametersTest, "a", V, I), 1));
+		asserter.assertDynamicProposal("seed", localVar(methodOf(delegateParametersTest, "b", V, J), 1));
 
-		asserter.assertNotProposed(localVar(method(delegateParametersTest, "b", "(I)V"), 1));
+		// method name: bar
+		asserter.assertNotProposed(localVar(methodOf(delegateParametersTest, "b", V, I), 1));
 
 		// Multiple parameters passed to the same method call within a single method def shouldn't be named
-		asserter.assertNotProposed(localVar(method(delegateParametersTest, "a", "(II)I"), 0));
-		asserter.assertNotProposed(localVar(method(delegateParametersTest, "a", "(II)I"), 1));
+		final MethodEntry addTwoAbs = methodOf(delegateParametersTest, "a", I, I, I);
+		asserter.assertNotProposed(localVar(addTwoAbs, 0));
+		asserter.assertNotProposed(localVar(addTwoAbs, 1));
 
-		asserter.assertNotProposed(localVar(method(delegateParametersTest, "a", "(IIII)I"), 0));
-		asserter.assertNotProposed(localVar(method(delegateParametersTest, "a", "(IIII)I"), 1));
-		asserter.assertNotProposed(localVar(method(delegateParametersTest, "a", "(IIII)I"), 2));
-		asserter.assertNotProposed(localVar(method(delegateParametersTest, "a", "(IIII)I"), 3));
+		final MethodEntry addFourAbs = methodOf(delegateParametersTest, "a", I, I, I, I, I);
+		asserter.assertNotProposed(localVar(addFourAbs, 0));
+		asserter.assertNotProposed(localVar(addFourAbs, 1));
+		asserter.assertNotProposed(localVar(addFourAbs, 2));
+		asserter.assertNotProposed(localVar(addFourAbs, 3));
 
 		// If multiple parameters would have the same name, don't name any of them
-		asserter.assertNotProposed(localVar(method(delegateParametersTest, "b", "(II)I"), 0));
-		asserter.assertNotProposed(localVar(method(delegateParametersTest, "b", "(II)I"), 1));
+		final MethodEntry twoAbs = methodOf(delegateParametersTest, "b", I, I, I);
+		asserter.assertNotProposed(localVar(twoAbs, 0));
+		asserter.assertNotProposed(localVar(twoAbs, 1));
 
 		final ClassEntry test1 = new ClassEntry(delegateParametersTest, "a");
 
-		asserter.assertDynamicProposal("val", localVar(method(test1, "<init>", "(I)V"), 1));
-		asserter.assertDynamicProposal("val", localVar(method(test1, "<init>", "(IJ)V"), 1));
-		asserter.assertDynamicProposal("val", localVar(method(test1, "<init>", "(IJI)V"), 1));
-		asserter.assertDynamicProposal("val", localVar(method(test1, "a", "(I)La/a/a$a;"), 0));
-		asserter.assertDynamicProposal("val", localVar(method(test1, "a", "(IJ)La/a/a$a;"), 0));
+		final MethodEntry constructorIJ = methodOf(test1, "<init>", V, I, J);
+		final MethodEntry constructorIJI = methodOf(test1, "<init>", V, I, J, I);
+		final MethodEntry createIJ = methodOf(test1, "a", TEST_1, I, J);
 
-		asserter.assertDynamicProposal("j", localVar(method(test1, "<init>", "(IJ)V"), 2));
-		asserter.assertDynamicProposal("j", localVar(method(test1, "<init>", "(IJI)V"), 2));
-		asserter.assertDynamicProposal("j", localVar(method(test1, "a", "(IJ)La/a/a$a;"), 1));
+		asserter.assertDynamicProposal("val", localVar(methodOf(test1, "<init>", V, I), 1));
+		asserter.assertDynamicProposal("val", localVar(constructorIJ, 1));
+		asserter.assertDynamicProposal("val", localVar(constructorIJI, 1));
+		// method name: create
+		asserter.assertDynamicProposal("val", localVar(methodOf(test1, "a", TEST_1, I), 0));
 
-		asserter.assertDynamicProposal("index", localVar(method(test1, "<init>", "(IJI)V"), 4));
+		asserter.assertDynamicProposal("val", localVar(createIJ, 0));
+
+		asserter.assertDynamicProposal("j", localVar(constructorIJ, 2));
+		asserter.assertDynamicProposal("j", localVar(constructorIJI, 2));
+		asserter.assertDynamicProposal("j", localVar(createIJ, 1));
+
+		asserter.assertDynamicProposal("index", localVar(constructorIJI, 4));
 
 		// Test dynamic renames
-		asserter.remapper().putMapping(new ValidationContext(null), localVar(method(test1, "<init>", "(IJI)V"), 2), new EntryMapping("silly"));
-		asserter.assertDynamicProposal("silly", localVar(method(test1, "<init>", "(IJ)V"), 2));
-		asserter.assertDynamicProposal("silly", localVar(method(test1, "a", "(IJ)La/a/a$a;"), 1));
+		asserter.remapper().putMapping(new ValidationContext(null), localVar(constructorIJI, 2), new EntryMapping("silly"));
+		asserter.assertDynamicProposal("silly", localVar(constructorIJ, 2));
+		asserter.assertDynamicProposal("silly", localVar(createIJ, 1));
 
-		asserter.remapper().putMapping(new ValidationContext(null), localVar(method(test1, "<init>", "(IJI)V"), 2), EntryMapping.OBFUSCATED);
-		asserter.assertDynamicProposal("j", localVar(method(test1, "<init>", "(IJ)V"), 2));
-		asserter.assertDynamicProposal("j", localVar(method(test1, "<init>", "(IJI)V"), 2));
-		asserter.assertDynamicProposal("j", localVar(method(test1, "a", "(IJ)La/a/a$a;"), 1));
+		asserter.remapper().putMapping(new ValidationContext(null), localVar(constructorIJI, 2), EntryMapping.OBFUSCATED);
+		asserter.assertDynamicProposal("j", localVar(constructorIJ, 2));
+		asserter.assertDynamicProposal("j", localVar(constructorIJI, 2));
+		asserter.assertDynamicProposal("j", localVar(createIJ, 1));
 
 		// Ensure names are also loaded from external classes
 		final ClassEntry getterSetterTest = new ClassEntry("a/a/b");
-		asserter.assertDynamicProposal("bound", localVar(method(getterSetterTest, "b", "(I)V"), 1));
+		// method name: foo
+		asserter.assertDynamicProposal("bound", localVar(methodOf(getterSetterTest, "b", V, I), 1));
 
 		// Names shouldn't be proposed for synthetic parameters (like Enum constructor name and ordinal)
 		final var enumTest = new ClassEntry("a/a/a/a");
-		final MethodEntry constructor = method(enumTest, "<init>", "(Ljava/lang/String;IZ)V");
+		final MethodEntry constructor = methodOf(enumTest, "<init>", V, STR, I, Z);
 
 		asserter.assertNotProposed(localVar(constructor, 1));
 		asserter.assertNotProposed(localVar(constructor, 2));
