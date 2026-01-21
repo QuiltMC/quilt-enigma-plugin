fd15155ec6b89d6e48479df1242e39e9395ebff3
Will <supersaiyansubtlety@gmail.com>
2026-01-20 20:47:45 -0800

    Update Enigma (#30)
    * update to Enigma 2.7.1
    
    * update deps to match enigma 2.7.1
    add junit launcher dep
    
    * update quilt_json_parser and dfu deps
    
    * add jspecify dep
    move to jspecify nullity annotations

--- src/main/java/org/quiltmc/enigma_plugin/index/simple_type_single/SimpleTypeFieldNamesRegistry.java
+++ src/main/java/org/quiltmc/enigma_plugin/index/simple_type_single/SimpleTypeFieldNamesRegistry.java
@@ -16,7 +16,7 @@
 
 package org.quiltmc.enigma_plugin.index.simple_type_single;
 
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 import org.quiltmc.enigma_plugin.util.CasingUtil;
 import org.quiltmc.parsers.json.JsonReader;
 import org.quiltmc.parsers.json.JsonToken;
